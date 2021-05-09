ac00e2971a05b3789ba489911312b2cf55d691a3
Arale Solo <arale@Arales-MacBook-Pro.local>
2021-05-09 12:03:50 -0700

    Second half of file .scala added

--- Gradient-boosted.scala
+++ Gradient-boosted.scala
@@ -24,3 +24,39 @@ val featureIndexer = new VectorIndexer()
 // Separamos los datos en dos partes, un llamado training con 70% y el otro test seta con 30%
 val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
 
+//Se entren al modelo GPT
+val gbt = new GBTClassifier()
+  .setLabelCol("indexedLabel")
+  .setFeaturesCol("indexedFeatures")
+  .setMaxIter(10)
+  .setFeatureSubsetStrategy("auto")
+
+//Comvertimos las etiqutas indezadas a etiquetas originales
+val labelConverter = new IndexToString()
+  .setInputCol("prediction")
+  .setOutputCol("predictedLabel")
+  .setLabels(labelIndexer.labels)
+
+//La cadena de los indezadores y GPT EN Pipeline
+val pipeline = new Pipeline()
+  .setStages(Array(labelIndexer, featureIndexer, gbt, labelConverter))
+
+//Se entreana al modelo. Esto también ejecuta los indexadores
+val model = pipeline.fit(trainingData)
+
+//Creamos las predicciones.
+val predictions = model.transform(testData)
+
+//Selecionamos las primeras 5 filas para desplegarlas
+predictions.select("predictedLabel", "label", "features").show(5)
+
+//Seleccionamos prediccion y calculo del error de prueba.
+val evaluator = new MulticlassClassificationEvaluator()
+  .setLabelCol("indexedLabel")
+  .setPredictionCol("prediction")
+  .setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictions)
+println(s"Test Error = ${1.0 - accuracy}")
+
+val gbtModel = model.stages(2).asInstanceOf[GBTClassificationModel]
+println(s"Learned classification GBT model:\n ${gbtModel.toDebugString}")
